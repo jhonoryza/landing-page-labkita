692ee1041d791f09225c3d3cbce6e4419b022d19
Fajar Sidik Priatna <jardik.oryza@gmail.com>
2020-10-26 10:49:28 +0700

    add produk page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { fade } from 'svelte/transition';
 	import successkid from "images/successkid.jpg";
 </script>
 
@@ -20,173 +21,180 @@
 	<title>Labkita</title>
 </svelte:head>
 
-<!-- Showcase -->
-<section class="showcase">
-	<div class="container grid">
-		<div class="showcase-text">
-			<h1>Jasa pembuatan aplikasi</h1>
-			<p>
-				Mobile Android, API, Situs Web statis/dinamis untuk individu,
-				lembaga atau perusahaan.
-			</p>
-			<a href="features.html" class="btn btn-outline">Lebih Lanjut</a>
-		</div>
-
-		<div class="showcase-form card">
-			<h2>Hubungi Kami</h2>
-			<form
-				name="contact"
-				netlify-honeypot="bot-field"
-				method="POST"
-				data-netlify="true">
-				<input type="hidden" name="form-name" value="contact" />
-				<p class="hidden">
-					<label>Don’t fill this out if you're human:
-						<input name="bot-field" /></label>
+<div
+    transition:fade
+	class="homepage">
+	<!-- Showcase -->
+	<section class="showcase">
+		<div class="container grid">
+			<div class="showcase-text">
+				<h1>Jasa pembuatan aplikasi</h1>
+				<p>
+					Mobile Android, API, Situs Web statis/dinamis untuk
+					individu, lembaga atau perusahaan.
 				</p>
-				<div class="form-control">
-					<input
-						type="text"
-						name="name"
-						placeholder="Nama"
-						required />
-				</div>
-				<div class="form-control">
-					<input
-						type="email"
-						name="email"
-						placeholder="Email"
-						required />
-				</div>
-				<div class="form-control">
-					<input
-						type="text"
-						name="message"
-						placeholder="Pesan"
-						required />
-				</div>
-				<div class="form-control">
-					<div
-						class="g-recaptcha"
-						data-sitekey="6LdSbdsZAAAAAIs_O4j-WQn7sqQOqvTU8lW8r6Bi" />
-				</div>
-				<input type="submit" value="Kirim" class="btn btn-primary" />
-			</form>
-		</div>
-	</div>
-</section>
-<!-- Stats -->
-<section class="stats">
-	<div class="container">
-		<h3 class="stats-heading text-center my-1">
-			Selamat datang di platform terbaik untuk membangun aplikasi dari
-			semua jenis dengan arsitektur dan penskalaan modern
-		</h3>
+				<a href="features.html" class="btn btn-outline">Lebih Lanjut</a>
+			</div>
 
-		<div class="grid grid-3 text-center my-4">
-			<div>
-				<i class="fas fa-server fa-3x" />
-				<h3>2</h3>
-				<p class="text-secondary">Developer</p>
-			</div>
-			<div>
-				<i class="fas fa-upload fa-3x" />
-				<h3>3</h3>
-				<p class="text-secondary">Published</p>
-			</div>
-			<div>
-				<i class="fas fa-project-diagram fa-3x" />
-				<h3>4</h3>
-				<p class="text-secondary">Projects</p>
+			<div class="showcase-form card">
+				<h2>Hubungi Kami</h2>
+				<form
+					name="contact"
+					netlify-honeypot="bot-field"
+					method="POST"
+					data-netlify="true">
+					<input type="hidden" name="form-name" value="contact" />
+					<p class="hidden">
+						<label>Don’t fill this out if you're human:
+							<input name="bot-field" /></label>
+					</p>
+					<div class="form-control">
+						<input
+							type="text"
+							name="name"
+							placeholder="Nama"
+							required />
+					</div>
+					<div class="form-control">
+						<input
+							type="email"
+							name="email"
+							placeholder="Email"
+							required />
+					</div>
+					<div class="form-control">
+						<input
+							type="text"
+							name="message"
+							placeholder="Pesan"
+							required />
+					</div>
+					<div class="form-control">
+						<div
+							class="g-recaptcha"
+							data-sitekey="6LdSbdsZAAAAAIs_O4j-WQn7sqQOqvTU8lW8r6Bi" />
+					</div>
+					<input
+						type="submit"
+						value="Kirim"
+						class="btn btn-primary" />
+				</form>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+	<!-- Stats -->
+	<section class="stats">
+		<div class="container">
+			<h3 class="stats-heading text-center my-1">
+				Selamat datang di platform terbaik untuk membangun aplikasi dari
+				semua jenis dengan arsitektur dan penskalaan modern
+			</h3>
 
-<!-- Cli -->
-<!-- <section class="cli">
+			<div class="grid grid-3 text-center my-4">
+				<div>
+					<i class="fas fa-server fa-3x" />
+					<h3>2</h3>
+					<p class="text-secondary">Developer</p>
+				</div>
+				<div>
+					<i class="fas fa-upload fa-3x" />
+					<h3>3</h3>
+					<p class="text-secondary">Published</p>
+				</div>
+				<div>
+					<i class="fas fa-project-diagram fa-3x" />
+					<h3>4</h3>
+					<p class="text-secondary">Projects</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Cli -->
+	<!-- <section class="cli">
 	<div class="container grid">
 		<img src="asset/img/cli.png" alt="" /> -->
-<!-- <div class="card">
+	<!-- <div class="card">
 			<h3>Easy to use, cross platform CLI</h3>
 		</div> -->
-<!-- <div class="card">
+	<!-- <div class="card">
 			<h3>Deploy in seconds</h3>
 		</div>
 	</div>
 </section> -->
 
-<!-- Languages -->
-<section class="languages">
-	<h2 class="md text-center my-2">Support Teknologi</h2>
-	<div class="container flex">
-		<div class="card">
-			<h4>Node.js</h4>
-			<img src="asset/img/logos/node.png" alt="" />
+	<!-- Languages -->
+	<section class="languages">
+		<h2 class="md text-center my-2">Support Teknologi</h2>
+		<div class="container flex">
+			<div class="card">
+				<h4>Node.js</h4>
+				<img src="asset/img/logos/node.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Docker</h4>
+				<img src="asset/img/logos/docker-container-100.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Python</h4>
+				<img src="asset/img/logos/python.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Golang</h4>
+				<img src="asset/img/logos/go.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>PHP</h4>
+				<img src="asset/img/logos/php.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Laravel</h4>
+				<img src="asset/img/logos/laravel-100.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>HTML5</h4>
+				<img src="asset/img/logos/html-5-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>CSS3</h4>
+				<img src="asset/img/logos/css3-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Javascript</h4>
+				<img src="asset/img/logos/javascript-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Svelte</h4>
+				<img src="asset/img/logos/svelte-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Kotlin</h4>
+				<img src="asset/img/logos/kotlin-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Flutter</h4>
+				<img src="asset/img/logos/flutter-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>AWS</h4>
+				<img src="asset/img/logos/aws-96.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>GCP</h4>
+				<img src="asset/img/logos/google-150.png" alt="" />
+			</div>
+			<div class="card">
+				<h4>Digitalocean</h4>
+				<img src="asset/img/logos/digitalocean-100.png" alt="" />
+			</div>
 		</div>
-		<div class="card">
-			<h4>Docker</h4>
-			<img src="asset/img/logos/docker-container-100.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Python</h4>
-			<img src="asset/img/logos/python.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Golang</h4>
-			<img src="asset/img/logos/go.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>PHP</h4>
-			<img src="asset/img/logos/php.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Laravel</h4>
-			<img src="asset/img/logos/laravel-100.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>HTML5</h4>
-			<img src="asset/img/logos/html-5-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>CSS3</h4>
-			<img src="asset/img/logos/css3-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Javascript</h4>
-			<img src="asset/img/logos/javascript-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Svelte</h4>
-			<img src="asset/img/logos/svelte-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Kotlin</h4>
-			<img src="asset/img/logos/kotlin-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Flutter</h4>
-			<img src="asset/img/logos/flutter-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>AWS</h4>
-			<img src="asset/img/logos/aws-96.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>GCP</h4>
-			<img src="asset/img/logos/google-150.png" alt="" />
-		</div>
-		<div class="card">
-			<h4>Digitalocean</h4>
-			<img src="asset/img/logos/digitalocean-100.png" alt="" />
-		</div>
-	</div>
-</section>
+	</section>
 
-<div class="kid">
-	<h1>Great!</h1>
-	<figure>
-		<img alt="Success Kid" src={successkid} />
-		<figcaption>Lets go!</figcaption>
-	</figure>
+	<div class="kid">
+		<h1>Great!</h1>
+		<figure>
+			<img alt="Success Kid" src={successkid} />
+			<figcaption>Lets go!</figcaption>
+		</figure>
+	</div>
 </div>
---
+++ src/routes/produk.svelte
@@ -0,0 +1,147 @@
+<script>
+    import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+    <title>Labkita</title>
+</svelte:head>
+
+<div transition:fade class="produkpage">
+    <!-- Head -->
+    <section class="features-head bg-dark py-3">
+        <div class="container grid">
+            <div>
+                <h1 class="xl">Produk Kami</h1>
+                <p class="lead">
+                    Berikut merupakan beberapa produk yg pernah kami kerjakan
+                </p>
+            </div>
+            <img src="asset/img/server.png" alt="" />
+        </div>
+    </section>
+
+    <!-- Sub head -->
+    <section class="features-sub-head bg-light py-3">
+        <div class="container grid">
+            <div>
+                <h1 class="md">Blog</h1>
+                <p>
+                    merupakan sebuah website personal blog dibangun menggunakan
+                    framework laravel dan tailwindcss
+                </p>
+                <a href="https://bloglama.labkita.my.id">Demo dapat dilihat
+                    disini</a>
+            </div>
+            <img src="https://i.ibb.co/ZYk2xvs/p1.png" alt="" />
+        </div>
+        <div class="container grid">
+            <div>
+                <h1 class="md">Git Self Hosted</h1>
+                <p>
+                    merupakan sebuah website repository code berbasis git,
+                    dibangun menggunakan gitea berbasis go
+                </p>
+                <a href="https://gitea.labkita.my.id">Demo dapat dilihat
+                    disini</a>
+            </div>
+            <img src="https://i.ibb.co/qYz8SDN/p2.png" alt="" />
+        </div>
+        <div class="container grid">
+            <div>
+                <h1 class="md">Cloud Storage Selft Hosted</h1>
+                <p>
+                    merupakan sebuah website penyimpanan cloud dibanugn menggunakan
+                    owncloud berbasis php
+                </p>
+                <a href="https://cloud.labkita.my.id">Demo dapat dilihat
+                    disini</a>
+            </div>
+            <img src="https://i.ibb.co/gRCPGf0/p3.png" alt="" />
+        </div>
+        <div class="container grid">
+            <div>
+                <h1 class="md">Shop (On Development)</h1>
+                <p>
+                    merupakan sebuah website personal online shop dibangun menggunakan
+                    framework laravel dan bootstrap.
+                    payment dapat menggunakan: whatsapp redirect atau midtrans
+                </p>
+                <a href="https://shop.labkita.my.id">Demo dapat dilihat
+                    disini</a>
+            </div>
+            <img src="https://i.ibb.co/98bvPpn/p4.png" alt="" />
+        </div>
+        <div class="container grid">
+            <div>
+                <h1 class="md">Story</h1>
+                <p>
+                    merupakan sebuah website personal story dibangun menggunakan
+                    framework laravel
+                </p>
+                <a href="https://stories.labkita.my.id">Demo dapat dilihat
+                    disini</a>
+            </div>
+            <img src="https://i.ibb.co/3mSD2SD/p5.png" alt="" />
+        </div>
+        <div class="container grid">
+            <div>
+                <h1 class="md">Mobile</h1>
+                <p>
+                    berikut aplikasi mobile android yg pernah kami develop
+                </p>
+                <a href="https://play.google.com/store/apps/dev?id=9101982290652990628">List Aplikasi disini</a>
+            </div>
+            <img src="https://i.ibb.co/q7HgtXy/p6.png" alt="" />
+        </div>
+    </section>
+    <!-- 
+    <section class="features-main my-2">
+        <div class="container grid grid-3">
+            <div class="card flex">
+                <i class="fas fa-server fa-3x" />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptas nemo saepe corrupti et dolorem voluptate at,
+                    nostrum rem voluptatem ab alias quia molestiae ea, libero
+                    nihil deserunt. Molestias, eum? Necessitatibus!
+                </p>
+            </div>
+            <div class="card flex">
+                <i class="fas fa-network-wired fa-3x" />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Ducimus recusandae perferendis culpa, reiciendis a itaque
+                    debitis qui vel dignissimos ipsum!
+                </p>
+            </div>
+            <div class="card flex">
+                <i class="fas fa-laptop-code fa-3x" />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Debitis, magnam.
+                </p>
+            </div>
+            <div class="card flex">
+                <i class="fas fa-database fa-3x" />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Vero, a!
+                </p>
+            </div>
+            <div class="card flex">
+                <i class="fas fa-power-off fa-3x" />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Debitis, magnam.
+                </p>
+            </div>
+            <div class="card flex">
+                <i class="fas fa-upload fa-3x" />
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Debitis, magnam.
+                </p>
+            </div>
+        </div>
+    </section> -->
+</div>
